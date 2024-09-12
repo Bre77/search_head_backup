8d58cdbf2d7dadf509f129f1d921fc0380f2423c
Brett <brett.whynot@gmail.com>
2024-09-12 11:48:17 +1000

    v1.0.4

--- .build.sh
+++ .build.sh
@@ -4,4 +4,4 @@ OUTPUT="${1:-search_head_backup.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
 cd ..
-tar -cpzf $OUTPUT --exclude=.* --overwrite SearchHeadBackup 
+tar -cpzf $OUTPUT --exclude=.* --overwrite search_head_backup 
